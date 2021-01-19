d089d79dba05abc6782ff15e237e5d47c810f895
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-19 11:34:31 -0800

    before hotwire

---
+++ app/javascript/trix-editor-overrides.js
@@ -0,0 +1,8 @@
+
+window.addEventListener("trix-file-accept", function(event) {
+  const maxFileSize = 1024 * 1024 // 1MB 
+  if (event.file.size > maxFileSize) {
+    event.preventDefault()
+    alert("Only support attachment files upto size 1MB!")
+  }
+})
