a8e9df44d2be6cc95fd0018ea4f69e7e69904d38
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-20 09:54:39 -0800

    who to follow

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,6 +14,6 @@ module ApplicationHelper
   end
 
   def people_to_follow
-    render partial: 'users/user', collection: User.sort_by_followers
+    render partial: 'users/user', collection: (User.sort_by_followers - [current_user])
   end
 end
