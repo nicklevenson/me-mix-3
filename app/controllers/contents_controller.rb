0e5b33c19d63f6d3d45091d178c3cd90cbe3c177
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-20 12:41:18 -0800

    added refactoring

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,6 +3,6 @@ class ContentsController < ApplicationController
   def destroy
     @mix = Mix.find(params[:mix_id])
     @mix.contents.delete(Content.find(params[:id]))
-    redirect_to "#{params[:lasturl]}"
+    redirect_to request.referrer
   end
 end
