7d82cb82935b7a2cda2e06fdd2021b1488117e92
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-20 08:02:34 -0800

    added to feed

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,5 +2,6 @@ class FeedController < ApplicationController
   before_action :logged_in?
   def index
     @mixes = current_user.feed 
+    
   end
 end
--- app/helpers/feed_helper.rb
+++ app/helpers/feed_helper.rb
@@ -1,2 +1,8 @@
 module FeedHelper
+  def no_feed 
+    if @mixes.empty? 
+      content_tag :h1, "This is where you'll see mixes by people you have followed. So go follow some people!" 
+      render partial: 'users/user', collection: User.all
+    end
+  end
 end
