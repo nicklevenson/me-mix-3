9fa888cee1096bd8c06ac1dfdf0f1c57c8acde81
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-20 12:16:59 -0800

    added if logged out features

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,7 +1,11 @@
 class FeedController < ApplicationController
-  before_action :logged_in?
+  # before_action :logged_in?
   def index
-    @mixes = current_user.feed 
+    if current_user
+      @mixes = current_user.feed
+    else
+      @mixes = Mix.most_recent
+    end
     
   end
 end
