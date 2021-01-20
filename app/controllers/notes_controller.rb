9ff7a6c7016820a87061f1999e49266a14f354f7
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-20 09:05:46 -0800

    style

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,6 +2,6 @@ class NotesController < ApplicationController
   def destroy
     @mix = Mix.find(params[:mix_id])
     @mix.notes.destroy(Note.find(params[:id]))
-    redirect_to "#{params[:lasturl]}"
+    redirect_to request.referrer
   end
 end
