61355e8bf664934ddf9406436da0177e2d9b9a89
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-20 18:40:08 -0800

    card style

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def profile_image(user)
     if user.avatar.attached?
-      "<div class='profile-image rounded'><img src='#{(url_for(user.avatar))}' class='rounded' width='120'></div>".html_safe
+      "<div class='profile-image'><img src='#{(url_for(user.avatar))}' class='rounded' width='120'></div>".html_safe
     else
-      "<div class='profile-image rounded'><img src='#{user.image}' class='rounded' width='120'></div>".html_safe
+      "<div class='profile-image'><img src='#{user.image}' class='rounded' width='120'></div>".html_safe
     end
   end
 
