06e88c4ef3acbc8456d0ab883e8a8a5624a6c7ff
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-22 20:30:43 -0800

    added all default

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   before_action :logged_in?
   def index
     @results = filter || []
-    @last_selected = params[:filter]   
+    @last_selected = params[:filter]
   end
 end
 
