31821a884027cdbcf2748ade6e95009b1013af95
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-30 13:59:36 -0800

    comments and contents

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,5 +1,10 @@
 class ContentsController < ApplicationController
   before_action :logged_in?
+
+  def show 
+    @content = Content.find(params[:id])
+    @mixes = @content.mixes
+  end
   def destroy
     @mix = Mix.find(params[:mix_id])
     @mix.contents.delete(Content.find(params[:id]))
