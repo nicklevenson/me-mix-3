e09ce5aabeed7038221632f4d30738c83ac34597
Nicholas Levenson <nicklevenson@gmail.com>
2021-04-22 10:01:16 -0700

    index is free of login

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   include WelcomeHelper
-  before_action :logged_in?
+  # before_action :logged_in?
   def index
     @results = filter || []
     @last_selected = params[:filter]
