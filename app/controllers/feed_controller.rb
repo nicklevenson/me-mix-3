c9af8d6e57c0379cef02be300e1700a1e6d656ac
Nicholas Levenson <nicklevenson@gmail.com>
2021-04-25 19:16:19 -0700

    feed faster

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,11 +1,11 @@
 class FeedController < ApplicationController
   before_action :logged_in?
   def index
-    if current_user
+    # if current_user
       @mixes = current_user.feed
-    else
-      @mixes = Mix.most_recent
-    end
+    # else
+    #   @mixes = Mix.most_recent
+    # end
     
   end
 end
