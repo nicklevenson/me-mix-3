5163b1e463341ea138e9c0d4ec08469a5382d5f6
Nicholas Levenson <nicklevenson@gmail.com>
2021-04-27 08:23:28 -0700

    follow user query

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,7 +47,7 @@ class User < ApplicationRecord
   end
   
   def not_followed_yet
-    User.order(updated_at: :desc)[0..8].select{|user| !user.followed_by.include?(self) && user != self}
+    User.all.sort_by{|u|u.mixes.length}[0..8].select{|user| !user.followed_by.include?(self) && user != self}.reverse()
   end
 
   def liked_mixes
